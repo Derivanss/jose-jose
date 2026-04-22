0523aa572033d19a67edcc013635f694e48233e0
Derivan <derivans.vs@gmail.com>
2026-04-21 19:53:01 -0600

    Añadir correciones
    Mover partes cruciales del archivo y crear independencias para renderizado exclusivo en gammas distintas (lo modifique pa celulares un poco)

---
+++ script.js
@@ -0,0 +1,8 @@
+function alerta_compra() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const tel = document.getElementById("tel").value;
+    const album =
+
+    alert("Wenas"+name)
+}
