ec68bab31217e098d3ae111a66f6009a7e27f708
Derivan <derivans.vs@gmail.com>
2026-04-28 19:23:21 -0600

    Correciones menores y form
    Agregar ciertas funciones puntuales y css. Añadir la funcionalidad del forms

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function alerta_compra() {
+let ventanaModal = document.getElementById("ventana-modal"),
+    textoModal = document.getElementById("texto-modal");
+
+function alertaCompra(event){
+    event.preventDefault();
+
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const tel = document.getElementById("tel").value;
-    const album =
+    const album = document.getElementById("album").value;
 
-    alert("Wenas"+name)
+    textoModal.innerHTML = "Estimado: <u>"+name+"</u> su pedido se ha hecho, se enviará la información relacionada al email: <u>"+email+".</u><br>Usted Compró el album: "+album
+
+    ventanaModal.showModal();
+}
+
+function cerrarVentana() {
+    ventanaModal.close();
 }
